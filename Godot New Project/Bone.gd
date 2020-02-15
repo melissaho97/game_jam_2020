27eed2c0021c4100ecff32ab1fdc4b303f5566b9
2425789 <2425789f@student.gla.ac.uk>
2020-02-15 22:17:10 +0000

    requires dog

--- Godot New Project/Bone.gd
+++ Godot New Project/Bone.gd
@@ -1,12 +1,17 @@
 extends RigidBody2D
-var x=0
+var y=0
 func _unhandled_input(event):
-	if event is InputEventMouseButton and event.pressed and x==0:
+	if event is InputEventMouseButton and event.pressed and y==0:
 		if event.button_index == BUTTON_LEFT:
-			x=1
+			y=1
 			var mo = (get_global_mouse_position()-get_position())
 			print(mo)
 			apply_impulse(Vector2(),mo*1.5)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+func _on_Visibility_screen_exited():
+	pass
+func _on_Node2D_sig():
+	y=0
+	queue_free()
--- Godot New Project/le work.gd
+++ Godot New Project/le work.gd
@@ -1,15 +1,21 @@
 extends Node2D
 var Bone = load("res://Bone.tscn")
 var x = 0
-
+signal sig
+var b = Bone.instance()
 func _ready():
 	OS.set_window_size(Vector2(1920, 1080))
-	set_process_input(true)
 
 func _unhandled_input(event):
-	if event is InputEventMouseButton and event.pressed and x == 0:
+	if event is InputEventMouseButton and event.pressed:
 		if event.button_index == BUTTON_LEFT:
-			pass
+			x=1
+		elif x==1:
+			if event.button_index == BUTTON_RIGHT:
+				emit_signal("sig")
+				remove_child(b)
+				b = Bone.instance()
+				b.position = Vector2(350,700)
+				add_child(b)
+				x=0
 			
-func _process(delta):
-	pass
