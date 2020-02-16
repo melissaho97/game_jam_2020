3a48722eb970727191ad39aa55ae2820f98629f4
TheShadowOfChernobyl <alex@Lain>
2020-02-16 02:02:49 +0000

    Almost final version

--- Godot New Project/Scences/DogWalking/Actors/Actor.gd
+++ Godot New Project/Scences/DogWalking/Actors/Actor.gd
@@ -4,7 +4,7 @@ class_name Actor
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-export var speed: = Vector2(300.0, 1000.0)
+export var speed: = Vector2(300.0, 600.0)
 export var gravity: = 3000.0
 var _velocity: = Vector2.ZERO
 # Called when the node enters the scene tree for the first time.
